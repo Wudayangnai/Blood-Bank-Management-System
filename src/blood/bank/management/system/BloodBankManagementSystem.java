423018f16b73dbba45ab428dcd7b11e503d23a81
Wudayangnai <udayangani97nibm@gmail.com>
2024-10-19 15:53:14 +0530

    Add files via upload

---
+++ src/blood/bank/management/system/BloodBankManagementSystem.java
@@ -0,0 +1,13 @@
+
+package blood.bank.management.system;
+
+
+public class BloodBankManagementSystem {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login loginform = new Login();
+        loginform.setVisible(true);
+    }
+    
+}
